f046907a6add75f523df5d6e112c9e42f80951d2
vmanolas <vmanolas@vivantehealth.com>
2017-10-27 09:46:52 +0300

    lecture 14: Spring Data JPA Repositories

---
+++ spring5webapp1-maven-spring200M4/src/main/java/tagv/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -0,0 +1,10 @@
+package tagv.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import tagv.springframework.spring5webapp.model.Author;
+
+/**
+ * Copied by vm on 10/27/2017
+ */
+public interface AuthorRepository extends CrudRepository<Author, Long>{
+}
